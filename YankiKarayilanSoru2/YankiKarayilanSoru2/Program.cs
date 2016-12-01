28c03e1a49134859105d76bbe9e267a4fba86208
mehmet.karayilan <mehmet.karayilan@ALS-YG39104-J14>
2016-12-01 11:45:12 +0200

    update

---
+++ YankiKarayilanSoru2/YankiKarayilanSoru2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YankiKarayilanSoru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region [1,n] kadarOlanSayilarinToplami
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int toplam = 0;
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    toplam += i;
+            //    Console.WriteLine("Sayi: {0} \t toplam: {1}", i, toplam);
+            //}
+
+            //Console.WriteLine("toplam: " + toplam);
+            //Console.ReadKey();
+
+            #endregion
+
+            #region [1,n] kadarOlanSayılarınTerstenToplamı
+
+            //Console.WriteLine("İstediginiz Sayiyi Girin");
+
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int toplam = 0;
+
+            //for (int i = n; i >= 1; i--)
+            //{
+            //    toplam += i;
+            //    Console.WriteLine("Sayi: {0} \t toplam: {1}", i, toplam);
+            //}
+
+            //Console.WriteLine("toplam: " + toplam);
+            //Console.ReadKey();
+            #endregion
+
+            #region SekilCizim
+
+            //Console.Write("Yüksekliği Girin : ");
+            //int yukseklik = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Genişliği Girin : ");
+            //int genislik = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= yukseklik; i++)
+            //{
+            //    for (int j = 1; j <= genislik; j++)
+            //    {
+            //        if (i == 1 || i == yukseklik)
+            //           Console.Write("*");
+            //        else
+            //            if (j == 1 || j == genislik)
+            //            Console.Write("*");
+            //        else
+            //            Console.Write(" ");
+            //    }
+            //   Console.WriteLine();
+            //}
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
